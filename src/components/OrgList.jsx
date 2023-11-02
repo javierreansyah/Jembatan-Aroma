9c9a0f49979a07da01b947dfc32b1c233901eab0
Javier <javierreansyah@gmail.com>
2023-11-02 22:49:03 +0700

    {javier}sidebar in yayasan and signup

--- src/components/OrgList.jsx
+++ src/components/OrgList.jsx
@@ -13,84 +13,89 @@ const OrgList = () => {
     <div className="container">
       <h1 className="my-4 text-4xl font-bold">Daftar Yayasan</h1>
 
-      <div className="mb-6 w-full rounded-3xl bg-wb-lightgray2 p-6">
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-bold">Filter</h1>
-          <button
-            onClick={toggleFilter}
-            className="rounded-2xl bg-wb-redorange px-4 py-1 text-wb-white"
-          >
-            {isFilterOpen ? "Tutup" : "Tampilkan"}
-          </button>
-        </div>
+      <div className="xl:flex xl:gap-10">
+        <div className="mb-6 w-full rounded-3xl bg-wb-lightgray2 p-6 xl:mb-0 xl:w-96">
+          <div className="flex items-center justify-between">
+            <h1 className="text-2xl font-bold">Filter</h1>
+            <button
+              onClick={toggleFilter}
+              className="rounded-2xl bg-wb-redorange px-4 py-1 text-wb-white xl:hidden"
+            >
+              {isFilterOpen ? "Tutup" : "Tampilkan"}
+            </button>
+          </div>
 
-        <div className={`${!isFilterOpen ? "hidden" : ""}`}>
-          <h1 className="text-md mt-6 font-bold">Kategori</h1>
-          <hr className="my-2 w-full border-t border-gray-300" />
-          <Clickable id="panti-asuhan" type="checkbox" label="Panti Asuhan" />
-          <hr className="my-2 w-full border-t border-gray-300" />
-          <Clickable id="panti-jompo" type="checkbox" label="Panti Jompo" />
-          <hr className="my-2 w-full border-t border-gray-300" />
-          <Clickable
-            id="lembaga-permasyarakatan"
-            type="checkbox"
-            label="Lembaga Permasyarakatan"
-          />
-          <hr className="my-2 w-full border-t border-gray-300" />
-          <Clickable id="lain-lain" type="checkbox" label="Lain-Lain" />
-
-          <h1 className="text-md mt-4 font-bold">Status</h1>
-          <Clickable id="membutuhkan" type="checkbox" label="Membutuhkan" />
-          <hr className="my-2 w-full border-t border-gray-300" />
-          <Clickable
-            id="tidak-membutuhkan"
-            type="checkbox"
-            label="Tidak Membutuhkan"
-          />
-
-          <h1 className="text-md mt-4 font-bold">Menerima Makanan</h1>
-          <Clickable
-            id="halal"
-            name="menerima-makanan"
-            type="radio"
-            label="Halal"
-          />
-          <hr className="my-2 w-full border-t border-gray-300" />
-          <Clickable
-            id="non-halal"
-            name="menerima-makanan"
-            type="radio"
-            label="Non Halal"
-          />
-          <button
-            className="mt-2 rounded-2xl bg-wb-redorange px-4 py-1 text-wb-white"
-            onClick={toggleFilter}
-          >
-            Terapkan
-          </button>
-        </div>
-      </div>
-      <ul className="rounded-3xl bg-wb-lightgray2 px-8 py-1">
-        {orgData.map((org) => (
-          <li
-            key={org.id}
-            className="my-8 rounded-2xl bg-wb-white p-6 shadow-md sm:flex sm:gap-6"
-          >
-            <img
-              src={org.img}
-              alt=""
-              className="mb-4 w-full rounded-xl sm:mb-0 sm:w-48"
+          <div className={`${!isFilterOpen ? "hidden" : ""} xl:block`}>
+            <h1 className="text-md mt-6 font-bold">Kategori</h1>
+            <hr className="my-2 w-full border-t border-gray-300" />
+            <Clickable id="panti-asuhan" type="checkbox" label="Panti Asuhan" />
+            <hr className="my-2 w-full border-t border-gray-300" />
+            <Clickable id="panti-jompo" type="checkbox" label="Panti Jompo" />
+            <hr className="my-2 w-full border-t border-gray-300" />
+            <Clickable
+              id="lembaga-permasyarakatan"
+              type="checkbox"
+              label="Lembaga Permasyarakatan"
             />
-            <div>
-              <h2 className="text-xl font-semibold">{org.name}</h2>
-              <p>Alamat: {org.alamat}</p>
-              <p>Kategori: {org.kategori}</p>
-              <p>Status: {org.status}</p>
-              <p>Halal: {org.halal}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+            <hr className="my-2 w-full border-t border-gray-300" />
+            <Clickable id="lain-lain" type="checkbox" label="Lain-Lain" />
+
+            <h1 className="text-md mt-4 font-bold">Status</h1>
+            <Clickable id="membutuhkan" type="checkbox" label="Membutuhkan" />
+            <hr className="my-2 w-full border-t border-gray-300" />
+            <Clickable
+              id="tidak-membutuhkan"
+              type="checkbox"
+              label="Tidak Membutuhkan"
+            />
+
+            <h1 className="text-md mt-4 font-bold">Menerima Makanan</h1>
+            <Clickable
+              id="halal"
+              name="menerima-makanan"
+              type="radio"
+              label="Halal"
+            />
+            <hr className="my-2 w-full border-t border-gray-300" />
+            <Clickable
+              id="non-halal"
+              name="menerima-makanan"
+              type="radio"
+              label="Non Halal"
+            />
+            <button
+              className="mt-4 rounded-2xl bg-wb-redorange px-4 py-1 text-wb-white xl:hidden"
+              onClick={toggleFilter}
+            >
+              Terapkan
+            </button>
+            <button className="mt-4 hidden rounded-2xl bg-wb-redorange px-4 py-1 text-wb-white xl:block">
+              Terapkan
+            </button>
+          </div>
+        </div>
+        <ul className="rounded-3xl bg-wb-lightgray2 px-8 py-1 xl:w-full">
+          {orgData.map((org) => (
+            <li
+              key={org.id}
+              className="my-8 rounded-2xl bg-wb-white p-6 shadow-md sm:flex sm:gap-6"
+            >
+              <img
+                src={org.img}
+                alt=""
+                className="mb-4 w-full rounded-xl sm:mb-0 sm:w-48"
+              />
+              <div>
+                <h2 className="text-xl font-semibold">{org.name}</h2>
+                <p>Alamat: {org.alamat}</p>
+                <p>Kategori: {org.kategori}</p>
+                <p>Status: {org.status}</p>
+                <p>Halal: {org.halal}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -7,37 +7,43 @@ const SignUp = () => {
     { value: "yayasan", label: "Yayasan" },
   ];
   return (
-    <div className="container h-44">
-      <div className="my-4 flex justify-between gap-3 rounded-2xl bg-wb-lightgray p-2">
-        <div className="flex-1 rounded-xl bg-wb-red px-2 py-2 text-center text-xs font-bold text-wb-white lg:text-sm">
-          Informasi Lembaga
+    <div className="container">
+      <div className="xl:flex xl:gap-10">
+        <div className="my-4 flex w-full justify-between gap-3 rounded-2xl bg-wb-lightgray p-2 xl:my-0 xl:block xl:w-96 xl:p-8">
+          <h1 className="hidden text-3xl font-bold text-wb-gray xl:block">
+            Alur
+          </h1>
+          <hr className="my-4 hidden w-full border-t border-gray-200 xl:block" />
+          <div className="flex-1 rounded-xl bg-wb-red px-2 py-2 text-center text-xs font-bold text-wb-white lg:text-sm xl:mb-4">
+            Informasi Lembaga
+          </div>
+          <div className="flex-1 rounded-xl bg-wb-lightgray2 px-2 py-2 text-center text-xs font-bold text-wb-gray lg:text-sm xl:mb-4">
+            Identitas Pengelola
+          </div>
+          <div className="flex-1 rounded-xl bg-wb-lightgray2 px-2 py-2 text-center text-xs font-bold text-wb-gray lg:text-sm xl:mb-4">
+            Sertifikat
+          </div>
         </div>
-        <div className="bg-wb-lightgray2 flex-1 rounded-xl px-2 py-2 text-center text-xs font-bold text-wb-gray lg:text-sm">
-          Identitas Pengelola
-        </div>
-        <div className="bg-wb-lightgray2 flex-1 rounded-xl px-2 py-2 text-center text-xs font-bold text-wb-gray lg:text-sm">
-          Sertifikat
-        </div>
-      </div>
 
-      <div className="h-screen rounded-2xl bg-wb-lightgray p-8">
-        <h1 className="text-3xl font-bold text-wb-gray">
-          Isikan Informasi Lembaga Anda
-        </h1>
-        <hr className="my-4 w-full border-t border-gray-200" />
-        <form>
-          <Input
-            id="org-name"
-            type="text"
-            placeholder="Nama Lembaga"
-            label="Nama Lembaga"
-          />
-          <Select
-            id="jenis-lembaga"
-            label="Daftar Sebagai"
-            options={orgSelectOption}
-          />
-        </form>
+        <div className="h-screen w-full rounded-2xl bg-wb-lightgray p-8">
+          <h1 className="text-3xl font-bold text-wb-gray">
+            Isikan Informasi Lembaga Anda
+          </h1>
+          <hr className="my-4 w-full border-t border-gray-200" />
+          <form>
+            <Input
+              id="org-name"
+              type="text"
+              placeholder="Nama Lembaga"
+              label="Nama Lembaga"
+            />
+            <Select
+              id="jenis-lembaga"
+              label="Daftar Sebagai"
+              options={orgSelectOption}
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
