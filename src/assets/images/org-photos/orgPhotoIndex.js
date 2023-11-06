91f0cb0ee951f7f842885a0d27807af397fa1f06
herdiadam <mherdi347@gmail.com>
2023-11-06 15:47:05 +0700

    [herdi] added yayasan

--- src/assets/images/org-photos/orgPhotoIndex.js
+++ src/assets/images/org-photos/orgPhotoIndex.js
@@ -1,5 +1,21 @@
-import yayasanabdulbari from "./yayasanabdulbari.jpg";
-import yayasanlolipon from "./yayasanlolipon.jpeg";
-import yayasanmanarulhuda from "./yayasanmanarulhuda.jpg";
+import yayasanAbdulBari from "./yayasanabdulbari.jpg";
+import yayasanQolbunSalim from "./yayasan-qolbun-salim.jpg";
+import yayasanManarulHuda from "./yayasan-manarul-huda.jpg";
+import pantiAsuhanSunanAmpel from "./panti-asuhan-sunan-ampel.jpeg";
+import lembagaPermasyarakatan1Malang from "./lembaga-permasyarakatan-1-malang.jpg";
+import rumahSosialBelasKasih from "./rumah-sosial-belas-kasih.jpg";
+import griyaKasihSiloam from "./griya-kasih-siloam.jpeg";
+import lembagaPemasyarakatanPerempuanKelasIIAMalang from "./lapas-perempuan-kelas-IIA-malang.jpeg";
+import yayasanPantiKristenJatim from "./yayasan-panti-kristen-jatim.jpg";
 
-export { yayasanabdulbari, yayasanlolipon, yayasanmanarulhuda };
+export {
+  yayasanAbdulBari,
+  yayasanQolbunSalim,
+  yayasanManarulHuda,
+  pantiAsuhanSunanAmpel,
+  lembagaPermasyarakatan1Malang,
+  rumahSosialBelasKasih,
+  griyaKasihSiloam,
+  lembagaPemasyarakatanPerempuanKelasIIAMalang,
+  yayasanPantiKristenJatim,
+};
