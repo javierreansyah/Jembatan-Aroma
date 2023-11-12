b7eca13728402911312740f0c0fd387ee842bfac
Haikal Thoriq Athaya <haikalthrq@gmail.com>
2023-11-12 17:36:41 +0700

    [Haikal] Added Certificate image in account

--- src/components/account-components/Certificate.jsx
+++ src/components/account-components/Certificate.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import restaurantCertificate from "../../assets/images/sertifikatRestoran.png";
+import philantrophyCertificate from "../../assets/images/philantrophyCertificate.jpg";
 
 const Certificate = ({ display, userType }) => {
-  return <div className={`${display ? "block" : "hidden"}`}>Certificate</div>;
+  return (
+    <div className={`${display ? "block" : "hidden"}`}>
+      <div className="mb-8 h-fit w-full rounded-3xl bg-wb-lightgray p-8">
+        <h1 className="text-3xl font-bold text-wb-gray">
+          Sertifikat {userType === "restoran" ? "Restoran" : "Yayasan"}
+        </h1>
+        <hr className="my-4 w-full border-t border-gray-200" />
+        {userType === "restoran" && (
+          <div>
+            <img src={restaurantCertificate} className="my-4" />
+          </div>
+        )}
+        {userType === "yayasan" && (
+          <div>
+            <img src={philantrophyCertificate} className="my-4" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Certificate;
