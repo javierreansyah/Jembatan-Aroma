108b362c43d64a5aef698b353e5951ae245c596f
Haikal Thoriq Athaya <haikalthrq@gmail.com>
2023-11-12 18:09:32 +0700

    [Haikal] Added image in donor.jsx

--- src/components/Donor.jsx
+++ src/components/Donor.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import Solaria from "../assets/images/DonorIMG/solaria.png";
+import Grab from "../assets/images/DonorIMG/Grab.png";
+import JCO from "../assets/images/DonorIMG/JCO.png";
 
 const Donor = () => {
-  return(
+  return (
     <div className="container">
-      <div className="xl:flex xl:gap-10">
-        {/* <div className="mb-8 space-y-8 xl:w-96"> */}
-          <div className="h-fit w-full justify-between gap-3 rounded-3xl bg-wb-lightgray p-20 md:p-20 xl:p-20">
-            <h1 className="hidden text-3xl font-bold text-wb-gray xl:justify-center xl:items-center xl:flex">
-              Donatur Kami
-            </h1>
-          </div>
-        {/* </div> */}
+      <div className="mb-8 h-fit w-full rounded-3xl bg-transparent p-8">
+        <h1 className="text-center text-3xl font-bold text-wb-gray">
+          Donatur Kami
+        </h1>
+        <div className="my-4 columns-1 gap-10 px-8 py-8 md:columns-2 lg:columns-3 xl:columns-4">
+          <img src={Solaria} />
+          <img src={Grab} />
+          <img src={JCO} />
+        </div>
       </div>
     </div>
   );
