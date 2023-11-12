89003d1952eed18dd290926766f33a670c58fb06
fauzi.arnda <fauzi.aranda@gmail.com>
2023-11-12 18:53:31 +0700

    [fauzi] FAQ list

--- src/components/support/SupportCenter.jsx
+++ src/components/support/SupportCenter.jsx
@@ -15,20 +15,47 @@ const SupportCenter = () => {
 
   const SupportCenter = {
     1: {
-      question: "Lorem Ipsum Dolor Sis Asmet?",
+      question: "Apa itu Jembatan Aroma?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Jembatan Aroma adalah sebuah platform yang bertujuan untuk mengurangi pemborosan makanan dan memberdayakan restoran, rumah makan, dan bisnis kuliner lainnya untuk berpartisipasi dalam kegiatan filantropi dengan cara menyumbangkan makanan berlebih kepada yayasan amal dan lembaga kemanusiaan yang membutuhkan.",
     },
     2: {
-      question: "Lorem Ipsum Dolor Sis Asmet?",
+      question: "Untuk apa Jembatan Aroma dibuat?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Jembatan Aroma dibuat untuk mengatasi masalah pemborosan makanan, ketidaksetaraan dalam akses pangan, dan pengelolaan sampah makanan. Platform ini bertujuan untuk membangun jembatan antara sumber makanan berlebih dari restoran dan bisnis kuliner dengan komunitas yang membutuhkan."
     },
     3: {
-      question: "Lorem Ipsum Dolor Sis Asmet?",
+      question: "Bagaimana Jembatan Aroma mengurangi pemborosan makanan?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Jembatan Aroma membantu mengurangi pemborosan makanan dengan menghubungkan restoran dengan yayasan yang membutuhkan makanan. Restoran dapat menyumbangkan makanan berlebih kepada yayasan amal melalui platform ini, sehingga makanan yang seharusnya terbuang dapat dimanfaatkan oleh mereka yang membutuhkan."
     },
+    4: {
+      question: "Siapa yang dapat menggunakan Jembatan Aroma?",
+      answer:
+        "Restoran, rumah makan, dan bisnis kuliner lainnya dapat menggunakan Jembatan Aroma untuk menyumbangkan makanan berlebih. Yayasan amal dan lembaga-lembaga lainnya yang membutuhkan makanan juga dapat mendaftar dan berpartisipasi di dalam platform ini.",
+    },
+    5: {
+      question: "Apa langkah-langkah dalam SOP Keamanan Makanan di Jembatan Aroma?",
+      answer:
+        "Langkah-langkah melibatkan identifikasi makanan berlebih, penyortiran untuk penyimpanan, memastikan kebersihan makanan dan alat makan, pengemasan yang aman, koordinasi pengambilan atau pengantaran, dan konfirmasi dengan pihak yayasan.",
+    },
+    6: {
+      question: "Apakah ada biaya yang harus dibayar untuk menggunakan Jembatan Aroma?",
+      answer:
+        "Tidak, menggunakan Jembatan Aroma tidak memerlukan biaya. Platform ini dirancang untuk memfasilitasi donasi makanan berlebih dengan tujuan sosial dan lingkungan yang positif.",
+    },
+    7: {
+      question: "Bagaimana cara restoran atau yayasan bergabung dengan Jembatan Aroma?",
+      answer:
+        "Restoran atau yayasan dapat membuat akun di Jembatan Aroma melalui website ini. Lalu perlu melakukan proses login dan mengisi informasi yang diperlukan. Setelah itu, restoran dapat mulai menyumbangkan makanan dan yayasan dapat memperbarui status mereka tentang kebutuhan donasi."
+    },
+    8: {
+      question: "Apa saja keuntungan bagi restoran yang berpartisipasi dalam Jembatan Aroma?",
+      answer:
+        "Restoran yang berpartisipasi dalam Jembatan Aroma dapat meningkatkan tanggung jawab sosial dan lingkungan mereka. Selain itu, mereka dapat mengurangi pemborosan makanan dan berkontribusi pada upaya mengatasi ketidaksetaraan dalam akses pangan di Indonesia. Melalui platform ini, restoran juga dapat memperluas jaringan mereka.",
+    },
+    
+
   };
   return (
     <section className="container">
@@ -82,6 +109,47 @@ const SupportCenter = () => {
               <p className="my-3 text-sm font-medium text-wb-gray">
                 {SupportCenter[3].answer}
               </p>
+
+              <hr className="my-4 w-full border-t border-gray-200" />
+              <h2 className="text-xl font-bold text-wb-gray">
+                {SupportCenter[4].question}
+              </h2>
+              <p className="my-3 text-sm font-medium text-wb-gray">
+                {SupportCenter[4].answer}
+              </p>
+
+              <hr className="my-4 w-full border-t border-gray-200" />
+              <h2 className="text-xl font-bold text-wb-gray">
+                {SupportCenter[5].question}
+              </h2>
+              <p className="my-3 text-sm font-medium text-wb-gray">
+                {SupportCenter[5].answer}
+              </p>
+
+              <hr className="my-4 w-full border-t border-gray-200" />
+              <h2 className="text-xl font-bold text-wb-gray">
+                {SupportCenter[6].question}
+              </h2>
+              <p className="my-3 text-sm font-medium text-wb-gray">
+                {SupportCenter[6].answer}
+              </p>
+
+              <hr className="my-4 w-full border-t border-gray-200" />
+              <h2 className="text-xl font-bold text-wb-gray">
+                {SupportCenter[7].question}
+              </h2>
+              <p className="my-3 text-sm font-medium text-wb-gray">
+                {SupportCenter[7].answer}
+              </p>
+
+              <hr className="my-4 w-full border-t border-gray-200" />
+              <h2 className="text-xl font-bold text-wb-gray">
+                {SupportCenter[8].question}
+              </h2>
+              <p className="my-3 text-sm font-medium text-wb-gray">
+                {SupportCenter[8].answer}
+              </p>
+
             </div>
           )}
           {currentPage === 2 && (
